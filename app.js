f8cad4a441ca7a85aeea50d52264dc596c123344
FinestWay <rrai30430@gmail.com>
2024-01-13 23:08:24 +0530

    Changes in logic

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const boxes = document.querySelectorAll(".box")
-const resetBtn = document.querySelector("#reset")
-const newGameBtn = document.querySelector("#new-btn")
-const msgContainer = document.querySelector(".msg-container")
-const msg = document.querySelector("#msg")
+const boxes = document.querySelectorAll(".box");
+const resetBtn = document.querySelector("#reset");
+const newGameBtn = document.querySelector("#new-btn");
+const msgContainer = document.querySelector(".msg-container");
+const msg = document.querySelector("#msg");
 
-let turnO = true;   //playerX, playerY
+let turnO = true; // playerX, playerY
+let draw = false;
 
 const winningPattern = [
     [0, 1, 2],
@@ -14,45 +15,60 @@ const winningPattern = [
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
-]
-
+    [2, 4, 6],
+];
 
 const resetGame = () => {
-    turnO = true;
-    enabledBoxes();
-}
+    if ([...boxes].some((box) => box.innerText !== "")) {
+        turnO = true;
+        draw = false;
+        enabledBoxes();
+        let applyVal = document.querySelector("#msg");
+        applyVal.innerHTML = "Play Again";
+        applyVal.style.color = "white";
+        setTimeout(() => {
+            applyVal.innerHTML = "";
+        }, 2000);
+    }
+};
 
-
+const checkDraw = () => {
+    if (![...boxes].some((box) => box.innerText === "")) {
+        draw = true;
+        let applyText = document.querySelector("#msg");
+        applyText.innerHTML = "The Match is drawn";
+        disabledBoxes();
+    }
+};
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
         if (turnO === true) {
             box.innerHTML = "O";
-            turnO = false
+            turnO = false;
             box.disabled = true;
-        }
-        else {
+        } else {
             box.innerHTML = "X";
             turnO = true;
             box.disabled = true;
         }
         checkWinner();
-    })
-})
+        checkDraw();
+    });
+});
 
 const disabledBoxes = () => {
-    for(let box of boxes){
+    for (let box of boxes) {
         box.disabled = true;
     }
-}
+};
 
 const enabledBoxes = () => {
-    for(let box of boxes){
+    for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
-}
+};
 
 const checkWinner = () => {
     for (let pattern of winningPattern) {
@@ -60,27 +76,23 @@ const checkWinner = () => {
         let position2 = boxes[pattern[1]].innerText;
         let position3 = boxes[pattern[2]].innerText;
 
-
-
         if (position1 != "" && position2 != "" && position3 != "") {
             if (position1 === position2 && position2 === position3) {
                 let applyText = document.querySelector("#msg");
                 applyText.innerHTML = `Winner is Player ${position1}`;
                 disabledBoxes();
+                draw = false; 
             }
         }
-
     }
-}
+};
 
-newGameBtn.addEventListener("click",() => {
+newGameBtn.addEventListener("click", () => {
     resetGame();
     let applyText = document.querySelector("#msg");
     applyText.innerHTML = "";
-})
+});
 
 resetBtn.addEventListener("click", () => {
     resetGame();
-    let applyVal = document.querySelector("#msg");
-    applyVal.innerHTML = "Play Again";
-})
+});
